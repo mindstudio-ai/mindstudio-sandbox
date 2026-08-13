7e974dc9c8bbbdbc0644db17fef7e88477e9fce1
sean <sean@gometa.io>
2026-08-13 10:23:17 -0400

    up

--- src/server/wsHandlers/misc.ts
+++ src/server/wsHandlers/misc.ts
@@ -30,6 +30,4 @@ export const miscHandlers: Record<string, ActionHandler> = {
   getResources: async () => {
     return ctx.resourceMonitor?.collectNow() ?? {};
   },
-
-  ping: async () => ({ pong: true }),
 };
